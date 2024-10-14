98e1c6955f04c171a0dc569e64b33c5002d078e3
acciojob <akshay121817@gmail.com>
2024-10-14 14:41:25 +0530

    xyz

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,7 +16,5 @@ public class Main {
     public static void main(String[] args) {
         B b = new B();
         System.out.println(b.meth());
-        B b2 = new B();
-        System.out.println(b2.meth());
     } 
 }
